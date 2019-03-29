f5c99f9318ef71039c8116996b05ce74920bf84b
Likbess <likbes.nik@gmail.com>
2019-03-29 09:51:47 +0200

    complete giphy project with react

--- learnReact/giphy/src/app.jsx
+++ learnReact/giphy/src/app.jsx
@@ -10,29 +10,73 @@ class App extends React.Component {
     super(props);
     this.state = {
       data: [],
+      copy: [],
     };
     this.loadTrendingGIF = this.loadTrendingGIF.bind(this);
+    this.selectOneRandom = this.selectOneRandom.bind(this);
+    this.randomizeOrder = this.randomizeOrder.bind(this);
+    this.reverseOrder = this.reverseOrder.bind(this);
+    this.clearAll = this.clearAll.bind(this);
   }
 
-  loadTrendingGIF() {
+  loadTrendingGIF(e) {
     // eslint-disable-next-line prettier/prettier
+    e.preventDefault();
     fetch('http://api.giphy.com/v1/gifs/trending?api_key=yMcWHbNz3IByrlaS1RBcYNw1XmARTUWL')
       .then(data => data.json())
       .then(gifs => {
         const urls = gifs.data.reduce((arr, gifObj) => {
-          return [...arr, gifObj.url];
+          const url = gifObj.images.fixed_height.url;
+          return [...arr, url];
         }, []);
-        this.setState({ data: urls });
+        this.setState({ data: urls, copy: urls });
       });
   }
 
+  selectOneRandom(e) {
+    e.preventDefault();
+    // implicit coercion array to string
+    if (this.state.data != '') {
+      this.setState({ copy: [this.state.data[this.randomMinMax(0, 24)]] });
+    }
+  }
+
+  randomizeOrder(e) {
+    e.preventDefault();
+    if (this.state.data == '') return;
+    this.setState(prev => {
+      return { copy: prev.data.sort(() => Math.random() - .5) }
+    })
+  }
+
+  reverseOrder(e) {
+    e.preventDefault();
+    if (this.state.data != '') {
+      this.setState(prev => {
+        return { copy: prev.copy.reverse() }
+      });
+    }
+  }
+
+  clearAll(e) {
+    e.preventDefault();
+    this.setState({
+      data: [],
+      copy: [],
+    });
+  }
+
+  randomMinMax(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min
+  }
+
   render() {
-    const { data } = this.state;
+    const { copy } = this.state;
     return (
       <>
         <h1>
           Trending GIFs courtesy of the
-          <a href="https://github.com/Giphy/GiphyAPI">Giphy API</a>
+          <a href="https://github.com/Giphy/GiphyAPI"> Giphy API</a>
         </h1>
         <Button
           text="Load (or Reload) Trending GIFs"
@@ -40,13 +84,13 @@ class App extends React.Component {
         />
         <Button
           text="Select One at Random"
-          handleClick={this.loadTrendingGIF}
+          handleClick={this.selectOneRandom}
         />
-        <Button text="Randomize Order" handleClick={this.loadTrendingGIF} />
-        <Button text="Reverse Order" handleClick={this.loadTrendingGIF} />
-        <Button text="Clear All" handleClick={this.loadTrendingGIF} />
+        <Button text="Randomize Order" handleClick={this.randomizeOrder} />
+        <Button text="Reverse Order" handleClick={this.reverseOrder} />
+        <Button text="Clear All" handleClick={this.clearAll} />
         <div id="container">
-          {data.map(url => (
+          {copy.map(url => (
             <img src={url} alt="gif" key={url} />
           ))}
         </div>
