401a0dc5b7885d4d2c328baa73512b7f9faa57cf
Likbes <likbes.js@gmail.com>
2019-04-14 21:11:04 +0300

    delete old folders

--- learnReact/quoteMachine/src/index.jsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import QuoteGen from './QuoteGen';
-
-ReactDOM.render(<QuoteGen />, document.getElementById('root'));
--- learnReact/weather/src/index.jsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Weather from './Weather';
-
-ReactDOM.render(<Weather />, document.getElementById('root'));
