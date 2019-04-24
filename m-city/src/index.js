f89e6e6e5075c152a53229e9f2967fc6f0f5ca61
Likbes <likbes.js@gmail.com>
2019-04-24 07:34:12 +0300

    create header, footer, reuseble logo components

--- m-city/src/index.js
+++ m-city/src/index.js
@@ -1,5 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import { BrowserRouter as Router } from 'react-router-dom';
+import Routes from './routes';
+import './Resources/css/app.css';
+
+const App = () => {
+  return (
+    <Router>
+      <Routes />
+    </Router>
+  );
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
