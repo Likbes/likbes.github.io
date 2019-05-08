7fcd71147b909fccec2563c943e0abeea1a4420b
Likbes <likbes.js@gmail.com>
2019-05-08 09:35:57 +0300

    fix different port problem

--- waves/client/src/App.js
+++ waves/client/src/App.js
@@ -1,6 +1,13 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 export default class App extends Component {
+
+  componentDidMount() {
+    axios.get('/api/product/woods')
+      .then(res => console.log(res))
+  }
+
   render() {
     return (
       <div>
