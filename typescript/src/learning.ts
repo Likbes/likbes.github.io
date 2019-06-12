53dc374b263f97eaa5162f32e01c5f857fefad06
Likbes <likbes.js@gmail.com>
2019-06-12 23:15:22 +0300

    learn 1/2 of ts doc. from basic types to iterators.

--- typescript/src/learning.ts
+++ typescript/src/learning.ts
@@ -23,3 +23,61 @@ function printable(fn: Function): void {
     console.log(this);
   }
 }
+
+// static properties
+class Grid {
+  static origin = { x: 0, y: 0 };
+  calculateDistanceFromOrigin(point: { x: number, y: number }) {
+    let xDist = (point.x - Grid.origin.x);
+    let yDist = (point.y - Grid.origin.y);
+    return Math.sqrt(xDist * xDist + yDist * yDist) / this.scale;
+  }
+  constructor(public scale: number) { this.scale = scale }
+}
+
+let grid1 = new Grid(1.0);  // 1x scale
+let grid2 = new Grid(5.0);  // 5x scale
+
+console.log(grid1.calculateDistanceFromOrigin({ x: 10, y: 10 }));
+console.log(grid2.calculateDistanceFromOrigin({ x: 10, y: 10 }));
+
+class Greeter {
+  static standardGreeting = "Hello, there";
+  greeting?: string;
+
+  constructor() {
+
+  }
+
+  greet() {
+    if (this.greeting) {
+      return "Hello, " + this.greeting;
+    }
+    else {
+      return Greeter.standardGreeting;
+    }
+  }
+}
+
+let greeter1: Greeter;
+greeter1 = new Greeter();
+console.log(greeter1.greet());
+
+let greeterMaker: typeof Greeter = Greeter;
+greeterMaker.standardGreeting = "Hey there!";
+
+let greeter2: Greeter = new greeterMaker();
+console.log(greeter2.greet());
+
+// generics
+
+class GenericNumber<T> {
+  zeroValue?: T;
+  add?: (x: T, y: T) => T;
+}
+
+let myGenericNumber = new GenericNumber<number>();
+myGenericNumber.zeroValue = 0;
+myGenericNumber.add = function (x, y) { return x + y; };
+
+console.log(myGenericNumber);
