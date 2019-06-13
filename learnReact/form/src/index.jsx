d2882558c951ed81fb39fc70fb326d50470053b6
Likbes <likbes.js@gmail.com>
2019-06-13 17:01:48 +0300

    delete leaen react folder

--- learnReact/form/src/index.jsx
+++
@@ -1,6 +0,0 @@
-/* eslint-disable import/no-unresolved */
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Form from './Form';
-
-ReactDOM.render(<Form />, document.getElementById('root'));
